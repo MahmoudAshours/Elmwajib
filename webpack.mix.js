60f8ac69d066533ddb1576c8ebab07e9f56d5c4e
Mahmoud Ashour <mashour365@gmail.com>
2025-12-15 18:41:57 +0200

    Refactor webpack.mix.js to correct versioning syntax for CSS output

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,8 @@ const mix = require('laravel-mix');
 mix.options({
     processCssUrls: false
 });
-
 mix.js('resources/js/app.js', 'public/js')
-    .sass("resources/sass/app.scss", "public/css", {}, [
-        require("rtlcss")(),
-    ]).version(['public/css/app.css']);
+    .sass('resources/sass/app.scss', 'public/css', {}, [
+        require('rtlcss')(),
+    ])
+    .version();
